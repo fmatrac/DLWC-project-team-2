8530f623a3942f2f583a91788122902cfde19b3d
mikolajrojek <mikolajrojek@student.agh.edu.pl>
2026-05-24 20:53:36 +0200

    Modifications to main.py

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import sys
 from pathlib import Path
+import torch
 
 sys.path.append(str(Path(__file__).parent / "src" / "scraper"))
 sys.path.append(str(Path(__file__).parent / "src" / "fetcher"))
 sys.path.append(str(Path(__file__).parent / "src" / "market"))
 sys.path.append(str(Path(__file__).parent / "src" / "embeder"))
 sys.path.append(str(Path(__file__).parent / "src" / "data"))
+sys.path.append(str(Path(__file__).parent / "src" / "model"))
+sys.path.append(str(Path(__file__).parent / "src" / "training"))
+
 import scraper
 import fetcher
 import market
 import embeder
 from data import build_dataloaders
+from model import MarketCNN
+from training import train, validate
 
 INPUT_FILE  = Path("data/raw/raw_gdelt.csv")
 OUTPUT_FILE = Path("data/processed/scraped_articles.jsonl")
@@ -21,6 +27,10 @@ EMBEDED_FILE = Path("data/processed/embeded.jsonl")
 
 
 def main():
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Using device:", device)
+
     Path("data/raw").mkdir(parents=True, exist_ok=True)
     Path("data/processed").mkdir(parents=True, exist_ok=True)
 
@@ -48,13 +58,12 @@ def main():
         market_path    = "data/raw/raw_market.jsonl",
         embedding_path = "data/processed/embeded.jsonl",
     )
+    model = MarketCNN().to(device)
 
-    # Inspect a batch
-    for batch in train_loader:
-        x = batch["x"]   # (B, emb_dim)  FloatTensor
-        y = batch["y"]   # (B,)          FloatTensor  — next-day S&P return
-        print(x.shape, y.shape)
-        break
+    train(model, train_loader, device=device)
+
+    validate(model, val_loader, device)
+    
 
 
 if __name__ == "__main__":
